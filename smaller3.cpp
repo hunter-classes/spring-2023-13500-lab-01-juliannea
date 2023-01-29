2775e781468912df557f62a2db54b47351064bcb
juliannea <julianneaguilar74@gmail.com>
2023-01-29 14:51:47 -0500

    removed namespace std

--- smaller3.cpp
+++ smaller3.cpp
@@ -8,7 +8,6 @@ The program will ask the user to input 3 numbers and determine which one of them
 */
 
 #include <iostream>
-using namespace std;
 
 int main()
 {
@@ -16,23 +15,23 @@ int main()
     int num2; 
     int num3;
     int num;
-    cout << "Enter the first number\n";
-    cin >> num1;
-    cout << "Enter the second number\n";
-    cin >> num2;
-    cout << "Enter the thrid number\n";
-    cin >> num3;
+    std::cout << "Enter the first number\n";
+    std::cin >> num1;
+    std::cout << "Enter the second number\n";
+    std::cin >> num2;
+    std::cout << "Enter the thrid number\n";
+    std::cin >> num3;
 
     if(num1 < num2)
     {
         num = num1;
         if(num < num3)
         {
-            cout << "The smaller of the three is " << num << endl;
+            std::cout << "The smaller of the three is " << num << std::endl;
         }
         else 
         {
-            cout << "The smaller of the three is " << num3 << endl;
+            std::cout << "The smaller of the three is " << num3 << std::endl;
         }
 
     }
@@ -42,11 +41,11 @@ int main()
         num = num2;
         if(num < num3)
         {
-            cout << "The smaller number of the three is " << num << endl;
+            std::cout << "The smaller number of the three is " << num << std::endl;
         }
         else 
         {
-            cout << "The smaller of the three is " << num3 << endl;
+            std::cout << "The smaller of the three is " << num3 << std::endl;
         }
     }
     
