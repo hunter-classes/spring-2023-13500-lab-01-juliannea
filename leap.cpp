678d6972c28324515542f8ac1e3d216f3053d534
juliannea <julianneaguilar74@gmail.com>
2023-01-29 15:02:50 -0500

    removed namespace std leap

--- leap.cpp
+++ leap.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
-using namespace std;
 
 int main()
 {
     int year;
-    cout << "Enter a year\n";
-    cin >> year;
+    std::cout << "Enter a year\n";
+    std::cin >> year;
 
     if(year % 4 != 0)
     {
-        cout << "It's a common year\n";
+        std::cout << "It's a common year\n";
     }
 
     else if(year % 100 != 0)
     {
-        cout << "It's a leap year\n";
+        std::cout << "It's a leap year\n";
     }
 
     else if(year % 400 != 0)
     {
-        cout << "It's a common year\n";
+        std::cout << "It's a common year\n";
     }
 
     else
     {
-        cout << "It's a leap year\n";
+        std::cout << "It's a leap year\n";
     }
     return 0;
 }
